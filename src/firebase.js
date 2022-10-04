7e3998771d05f2d6052513ef7c4682a4d91b460b
John KK <johnkk1316@protonmail.com>
2022-10-04 15:20:18 +0300

    Added project files

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBgnDmGsNjKIFz3dC5g12o5Iym6kyJIFlc",
+
+    authDomain: "netfix-clone-d398b.firebaseapp.com",
+
+    projectId: "netfix-clone-d398b",
+
+    storageBucket: "netfix-clone-d398b.appspot.com",
+
+    messagingSenderId: "169717959171",
+
+    appId: "1:169717959171:web:72a3bad134ee75b3abdc36"
+    
+};    
